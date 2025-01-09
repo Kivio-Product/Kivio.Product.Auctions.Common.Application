d80b3cd8dc599d3ae9d882d2b62b6db27ac7e437
Valentina Arias <vaariasa@unal.edu.co>
2025-01-09 10:59:06 -0500

    feat: Add middleware to verify authorization

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Kivio-Product/Kivio.Product.Auctions.Common.Application
+
+go 1.23.3
---
+++ internal/application/handlers/middleware.go
@@ -0,0 +1,57 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+var (
+	AUTH_NEEDED = []string{
+		"getToken",
+		"getOffers",
+		"getPointOfSale",
+	}
+)
+
+func shouldCheckToken(route string) bool {
+	for _, p := range AUTH_NEEDED {
+		if strings.Contains(route, p) {
+			return true
+		}
+	}
+	return false
+}
+
+func CheckAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !shouldCheckToken(r.URL.Path) {
+			next.ServeHTTP(w, r)
+			return
+		}
+		
+		tokenString := r.Header.Get("Authorization")
+
+		userAPIURL := fmt.Sprintf("http://localhost:5050/verifyToken?token=%s", tokenString)
+
+		req, err := http.NewRequest("POST", userAPIURL, nil)
+		if err != nil {
+			http.Error(w, "Error creating request", http.StatusInternalServerError)
+			return
+		}
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			http.Error(w, "Error communicating with user API", http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
