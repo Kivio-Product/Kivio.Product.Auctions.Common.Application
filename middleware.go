4cec91fc7f91724608361dec5fca1f9aeaa31522
Valentina Arias <vaariasa@unal.edu.co>
2025-02-04 05:30:21 -0500

    feat: change CheckAuthMiddleware logic

--- middleware.go
+++ middleware.go
@@ -1,10 +1,23 @@
 package middleware
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"strings"
+
+	"github.com/Kivio-Product/Kivio.Product.Auctions.Common.Application/helpers"
+	"github.com/golang-jwt/jwt"
+	"github.com/joho/godotenv"
 )
+
+type AppMetadata struct {
+	Provider  string   `json:"provider"`
+	Providers []string `json:"providers"`
+	Role      string   `json:"role"`
+}
 
 var (
 	NO_AUTH_NEEDED = []string{
@@ -29,36 +42,94 @@ func ConfigureCORS(next http.Handler) http.Handler {
 	})
 }
 
-func CheckAuthMiddleware(next http.Handler) http.Handler {
+func CheckAuthMiddleware(next http.Handler, allowedRoles []string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !shouldCheckToken(r.URL.Path) {
-			next.ServeHTTP(w, r)
+		// 	if !shouldCheckToken(r.URL.Path) {
+		// 		next.ServeHTTP(w, r)
+		// 		return
+		// 	}
+
+		// 	tokenString := r.Header.Get("Authorization")
+
+		// 	userAPIURL := fmt.Sprintf("http://localhost:5050/verifyToken?token=%s", tokenString)
+
+		// 	req, err := http.NewRequest("POST", userAPIURL, nil)
+		// 	if err != nil {
+		// 		http.Error(w, "Error creating request", http.StatusInternalServerError)
+		// 		return
+		// 	}
+
+		// 	client := &http.Client{}
+		// 	resp, err := client.Do(req)
+		// 	if err != nil {
+		// 		http.Error(w, "Error communicating with user API", http.StatusInternalServerError)
+		// 		return
+		// 	}
+		// 	defer resp.Body.Close()
+
+		// 	if resp.StatusCode != http.StatusOK {
+		// 		http.Error(w, "Invalid token", http.StatusUnauthorized)
+		// 		return
+		// 	}
+		// 	next.ServeHTTP(w, r)
+
+		tokenString := r.Header.Get("Access_token")
+		if tokenString == "" {
+			http.Error(w, "Missing token", http.StatusUnauthorized)
 			return
 		}
 
-		tokenString := r.Header.Get("Authorization")
-
-		userAPIURL := fmt.Sprintf("http://localhost:5050/verifyToken?token=%s", tokenString)
-
-		req, err := http.NewRequest("POST", userAPIURL, nil)
+		err := godotenv.Load(".env")
 		if err != nil {
-			http.Error(w, "Error creating request", http.StatusInternalServerError)
-			return
+			log.Fatal("Error loading .env file")
 		}
+		JWTSecret := os.Getenv("JWT_SECRET")
 
-		client := &http.Client{}
-		resp, err := client.Do(req)
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method %v", token.Header["alg"])
+			}
+			return []byte(JWTSecret), nil
+		})
+
 		if err != nil {
-			http.Error(w, "Error communicating with user API", http.StatusInternalServerError)
-			return
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
 			http.Error(w, "Invalid token", http.StatusUnauthorized)
 			return
 		}
-		next.ServeHTTP(w, r)
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			if appMetadata, exists := claims["app_metadata"]; exists {
+				jsonData, err := json.Marshal(appMetadata)
+				if err != nil {
+					http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+					return
+				}
+
+				var appMeta AppMetadata
+				err = json.Unmarshal(jsonData, &appMeta)
+				if err != nil {
+					log.Println("Error al parsear app_metadata:", err)
+					http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+					return
+				}
+
+				for _, role := range allowedRoles {
+					if appMeta.Role == role {
+						next.ServeHTTP(w, r)
+						return
+					}
+				}
+
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			} else {
+				http.Error(w, "Invalid token", http.StatusUnauthorized)
+				return
+			}
+		} else {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
 	})
 }
 
